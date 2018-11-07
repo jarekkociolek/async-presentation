d6f05ef46ae2e522196feae641a81b5f6c8edbdd
Jaroslaw Kociolek <jko@scalepoint.com>
2018-11-07 08:46:52 +0100

    init

---
+++ AsyncAwaitConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Task.Run(() => MainAsync(args)).Result;
+        }
+
+
+        static async Task<string> MainAsync(string[] args)
+        {
+            try
+            {
+                Task task = GetAsync("");
+
+                return "";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        static async Task GetAsync(string url)
+        {
+            await Task.Delay(100);
+            throw new Exception();
+        }
+    }
+}
